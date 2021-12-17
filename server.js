cc79104025c069bb3e85813e6aa3e968883e2def
jcbest <johnsonchau7@gmail.com>
2021-12-17 22:03:22 +1100

    add redirect

--- server.js
+++ server.js
@@ -4,14 +4,26 @@ const enforce = require('express-sslify');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(enforce.HTTPS());
+app.use(function (req, res, next) {
+  if (req.secure) 
+      return next();
+
+  var target = url.format({
+      protocol: 'https:',
+      host: req.hostname,
+      pathname: req.url
+  });
+
+  res.redirect(301, target);
+});
+
 app.use(express.json());
 
 // Your static site folder name
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname + '/build')));
 
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/build/index.html'));
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname + '/build/index.html'));
   });
 
 app.listen(port, ()=>{
